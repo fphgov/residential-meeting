0188161f2a7b629b49920dad1951f8d09c04e5cc
Horváth Tamás András <horvath.tamas.a@budapest.hu>
2020-07-27 16:46:57 +0200

    Mini changes

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 ## Participatory Budget
+
+Start project
+```
+docker-compose up -d
+```
+
+Install dependecies on backend
+```
+docker exec -it participatory_webapp composer install --no-ansi --no-dev --no-interaction --no-plugins --no-progress --no-scripts --no-suggest --optimize-autoloader
+```
+
+Migrate database from Doctrine
+```
+docker exec -it participatory_webapp composer db-update
+```
--- config/routes.php
+++ config/routes.php
@@ -7,29 +7,29 @@ use Mezzio\MiddlewareFactory;
 use Psr\Container\ContainerInterface;
 
 return static function (Application $app, MiddlewareFactory $factory, ContainerInterface $container) : void {
-    $app->get('/api/ping', App\Handler\PingHandler::class, 'api.ping');
+    $app->get('/app/api/ping', App\Handler\PingHandler::class, 'api.ping');
 
-    $app->post('/api/login', [
+    $app->post('/app/api/login', [
         Jwt\Handler\TokenHandler::class,
     ], 'api.login');
 
-    $app->get('/api/user', [
+    $app->get('/app/api/user', [
         Jwt\Handler\JwtAuthMiddleware::class,
         App\Middleware\UserMiddleware::class,
         App\Handler\User\ListHandler::class
     ], 'api.user');
 
-    $app->get('/api/project/{hashId}', [
+    $app->get('/app/api/project/{hashId}', [
         App\Handler\Project\GetHandler::class
     ], 'api.project.get');
 
-    $app->post('/api/project', [
+    $app->post('/app/api/project', [
         Jwt\Handler\JwtAuthMiddleware::class,
         App\Middleware\UserMiddleware::class,
         App\Handler\Project\AddHandler::class
     ], 'api.project.add');
-    
-    $app->get('/api/project', [
+
+    $app->get('/app/api/project', [
         App\Handler\Project\ListHandler::class
     ], 'api.project.all');
 };
