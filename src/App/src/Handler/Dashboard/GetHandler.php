e7038128d98072650566817ddacf87bef488617e
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2021-06-16 13:46:35 +0200

    Return offline votes count in dashboard query

--- src/App/src/Handler/Dashboard/GetHandler.php
+++ src/App/src/Handler/Dashboard/GetHandler.php
@@ -6,9 +6,11 @@ namespace App\Handler\Dashboard;
 
 use App\Entity\User;
 use App\Entity\Vote;
+use App\Entity\OfflineVote;
 use App\Service\SettingServiceInterface;
 use App\Repository\UserRepository;
 use App\Repository\VoteRepository;
+use App\Repository\OfflineVoteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Laminas\Diactoros\Response\JsonResponse;
 use Psr\Http\Message\ResponseInterface;
@@ -29,27 +31,33 @@ final class GetHandler implements RequestHandlerInterface
     /** @var VoteRepository **/
     private $voteRepository;
 
+    /** @var OfflineVoteRepository **/
+    private $offlineVoteRepository;
+
     public function __construct(
         EntityManagerInterface $em,
         SettingServiceInterface $settingService
     ) {
-        $this->em             = $em;
-        $this->settingService = $settingService;
-        $this->userRepository = $this->em->getRepository(User::class);
-        $this->voteRepository = $this->em->getRepository(Vote::class);
+        $this->em                    = $em;
+        $this->settingService        = $settingService;
+        $this->userRepository        = $this->em->getRepository(User::class);
+        $this->voteRepository        = $this->em->getRepository(Vote::class);
+        $this->offlineVoteRepository = $this->em->getRepository(OfflineVote::class);
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $setting    = $this->settingService->getRepository()->find(1);
-        $countUsers = $this->userRepository->count([]);
-        $countVotes = $this->voteRepository->count([]);
+        $setting           = $this->settingService->getRepository()->find(1);
+        $countUsers        = $this->userRepository->count([]);
+        $countVotes        = $this->voteRepository->count([]);
+        $countOfflineVotes = $this->offlineVoteRepository->count([]);
 
         return new JsonResponse([
             'settings' => $setting,
             'infos'    => [
-                'countUsers' => $countUsers,
-                'countVotes' => $countVotes,
+                'countUsers'        => $countUsers,
+                'countVotes'        => $countVotes,
+                'countOfflineVotes' => $countOfflineVotes,
             ]
         ]);
     }
