1d6b8c90bc0b69f21b059ffa8bfa5cf91709c4b4
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2022-01-19 03:00:16 +0100

    Add disabled check sonar lines

--- test/AppTest/Service/PBKDF2PasswordTest.php
+++ test/AppTest/Service/PBKDF2PasswordTest.php
@@ -16,7 +16,7 @@ final class PBKDF2PasswordTest extends TestCase
             . "4e574b19813e5e5af7ae936a1798b0c12f28b79bb761f32f94270b31"
             . "731f44bca37513855af9c08f7abf09e65bc46f9f1b25d39b31a657b5649c7bc8020e1486"
             . "ae854c5aefdb73a74e4ceb0acd96abee24ca68cf8c0403b7602952f0a0"
-            . "bf8662a4c83c4c28ecb0c282d2afe49e71870e260c07f419c9ddd3c63115694864b1e5";
+            . "bf8662a4c83c4c28ecb0c282d2afe49e71870e260c07f419c9ddd3c63115694864b1e5"; // NOSONAR
 
         $passwordModel = new PBKDF2Password($storedPassword, PBKDF2Password::PW_REPRESENTATION_STORABLE);
 
@@ -29,7 +29,7 @@ final class PBKDF2PasswordTest extends TestCase
     {
         $this->expectException(IllegalArgumentException::class);
 
-        $storedPassword = "Bad stored password";
+        $storedPassword = "Bad stored password"; // NOSONAR
 
         $passwordModel = new PBKDF2Password($storedPassword, PBKDF2Password::PW_REPRESENTATION_STORABLE);
     }
@@ -38,7 +38,7 @@ final class PBKDF2PasswordTest extends TestCase
     {
         $this->expectException(IllegalArgumentException::class);
 
-        $storedPassword = "PBKDF2WITHHMACSHA512:40000:90be05c0b062cdc94fd6124a88f95523";
+        $storedPassword = "PBKDF2WITHHMACSHA512:40000:90be05c0b062cdc94fd6124a88f95523"; // NOSONAR
 
         $passwordModel = new PBKDF2Password($storedPassword, PBKDF2Password::PW_REPRESENTATION_STORABLE);
     }
@@ -51,14 +51,14 @@ final class PBKDF2PasswordTest extends TestCase
             . "4e574b19813e5e5af7ae936a1798b0c12f28b79bb761f32f94270b31"
             . "731f44bca37513855af9c08f7abf09e65bc46f9f1b25d39b31a657b5649c7bc8020e14"
             . "86ae854c5aefdb73a74e4ceb0acd96abee24ca68cf8c0403b7602952f0a0"
-            . "bf8662a4c83c4c28ecb0c282d2afe49e71870e260c07f419c9ddd3c63115694864b1e5";
+            . "bf8662a4c83c4c28ecb0c282d2afe49e71870e260c07f419c9ddd3c63115694864b1e5"; // NOSONAR
 
         $passwordModel = new PBKDF2Password($storedPassword, PBKDF2Password::PW_REPRESENTATION_STORABLE);
     }
 
     public function testRecyclability()
     {
-        $password = "PASSWORD";
+        $password = "PASSWORD"; // NOSONAR
 
         $passwordModel  = new PBKDF2Password($password);
         $storedPassword = $passwordModel->getStorableRepresentation();
