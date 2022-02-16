dbae4057afd60e74aed41bb559f7c4e4dd9c2f5a
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2022-02-16 17:08:08 +0100

    Dinamic workflowstate status

--- src/App/src/Repository/IdeaRepository.php
+++ src/App/src/Repository/IdeaRepository.php
@@ -4,8 +4,41 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use App\Entity\CampaignInterface;
+use App\Entity\WorkflowState;
+use App\Entity\WorkflowStateInterface;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
+
+use function implode;
 
 final class IdeaRepository extends EntityRepository
 {
+    public function getWorkflowStates(string $campaign = '')
+    {
+        $qb = $this->createQueryBuilder('i');
+
+        $qb
+            ->where('i.workflowState NOT IN (:disableWorkflowStates)')
+            ->groupBy('i.workflowState');
+
+        if ($campaign != '') {
+            $qb->andWhere('i.campaign IN (:campaign)');
+            $qb->setParameter('campaign', $campaign);
+        }
+
+        $qb->setParameter('disableWorkflowStates', [
+            WorkflowStateInterface::STATUS_RECEIVED,
+            WorkflowStateInterface::STATUS_USER_DELETED,
+            WorkflowStateInterface::STATUS_TRASH,
+        ]);
+
+        $workflowStates = [];
+
+        foreach ($qb->getQuery()->getResult() as $idea) {
+            $workflowStates[] = $idea->getWorkflowState();
+        }
+
+        return $workflowStates;
+    }
 }
