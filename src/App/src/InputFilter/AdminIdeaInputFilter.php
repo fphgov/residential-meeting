8d83f3e5a6e058043683710f1c836bff129af3da
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2022-02-28 12:07:42 +0100

    Modify admin solution field

--- src/App/src/InputFilter/AdminIdeaInputFilter.php
+++ src/App/src/InputFilter/AdminIdeaInputFilter.php
@@ -32,5 +32,31 @@ class AdminIdeaInputFilter extends IdeaInputFilter
             'name'        => 'workflowStateExtra',
             'allow_empty' => true,
         ]);
+
+        $this->add([
+            'name'        => 'solution',
+            'allow_empty' => false,
+            'validators'  => [
+                new Validator\NotEmpty([
+                    'messages' => [
+                        Validator\NotEmpty::IS_EMPTY => 'A "Min szeretnél változtatni?" kitöltése kötelező',
+                        Validator\NotEmpty::INVALID  => 'Min szeretnél változtatni?: Hibás mező tipus',
+                    ],
+                ]),
+                new Validator\StringLength([
+                    'messages' => [
+                        Validator\StringLength::TOO_SHORT => 'Legalább %min% karaktert kell tartalmaznia a "Min szeretnél változtatni?" mezőnek',
+                        Validator\StringLength::TOO_LONG  => 'Kevesebb karaktert kell tartalmaznia a "Min szeretnél változtatni?" mezőnek mint: %max%',
+                        Validator\StringLength::INVALID   => 'Min szeretnél változtatni?: Hibás mező tipus. Csak szöveg fogadható el.',
+                    ],
+                    'min'      => 20,
+                    'max'      => 500,
+                ]),
+            ],
+            'filters'     => [
+                new Filter\StringTrim(),
+                new Filter\StripTags(),
+            ],
+        ]);
     }
 }
