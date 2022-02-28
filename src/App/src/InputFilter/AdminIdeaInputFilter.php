261cb179cbc8954baa21c1dd8fbb715a5887bc8c
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2022-02-28 13:18:46 +0100

    Editable max lenghts in input filter

--- src/App/src/InputFilter/AdminIdeaInputFilter.php
+++ src/App/src/InputFilter/AdminIdeaInputFilter.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\InputFilter;
 
 use Laminas\Db\Adapter\AdapterInterface;
+use Laminas\Filter;
+use Laminas\Validator;
 
 class AdminIdeaInputFilter extends IdeaInputFilter
 {
@@ -50,7 +52,33 @@ class AdminIdeaInputFilter extends IdeaInputFilter
                         Validator\StringLength::INVALID   => 'Min szeretnél változtatni?: Hibás mező tipus. Csak szöveg fogadható el.',
                     ],
                     'min'      => 20,
-                    'max'      => 500,
+                    'max'      => 1000,
+                ]),
+            ],
+            'filters'     => [
+                new Filter\StringTrim(),
+                new Filter\StripTags(),
+            ],
+        ]);
+
+        $this->add([
+            'name'        => 'description',
+            'allow_empty' => false,
+            'validators'  => [
+                new Validator\NotEmpty([
+                    'messages' => [
+                        Validator\NotEmpty::IS_EMPTY => 'A "Leírás" kitöltése kötelező',
+                        Validator\NotEmpty::INVALID  => 'Leírás: Hibás mező tipus',
+                    ],
+                ]),
+                new Validator\StringLength([
+                    'messages' => [
+                        Validator\StringLength::TOO_SHORT => 'Legalább %min% karaktert kell tartalmaznia a "Leírás" mezőnek',
+                        Validator\StringLength::TOO_LONG  => 'Kevesebb karaktert kell tartalmaznia a "Leírás" mezőnek mint: %max%',
+                        Validator\StringLength::INVALID   => 'Leírás: Hibás mező tipus. Csak szöveg fogadható el.',
+                    ],
+                    'min'      => 200,
+                    'max'      => 4000,
                 ]),
             ],
             'filters'     => [
