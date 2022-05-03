6e112ea6df762a83774b7f088c2ed1db9427a011
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2022-05-03 14:24:45 +0200

    Change custom email name

--- src/Mail/src/Helper/MailContentRawHelperInterface.php
+++ src/Mail/src/Helper/MailContentRawHelperInterface.php
@@ -9,5 +9,7 @@ use Mail\Model\EmailTemplateRenderModelInterface;
 
 interface MailContentRawHelperInterface extends EmailTemplateRenderModelInterface
 {
+    public const MAIL_CODE = 'custom';
+
     public function create(EmailContentModelInterface $emailContentModel, array $variables): self;
 }
