2093e71423c0ad558bf1ff7e6d38ffa04683aa0f
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2022-10-04 22:56:23 +0200

    Fixed depenency

--- src/App/src/Handler/User/ConfirmationHandlerFactory.php
+++ src/App/src/Handler/User/ConfirmationHandlerFactory.php
@@ -6,6 +6,8 @@ namespace App\Handler\User;
 
 use App\InputFilter\ProfilActivationInputFilter;
 use App\Service\UserServiceInterface;
+use App\Entity\Entity;
+use Doctrine\ORM\EntityManagerInterface;
 use Laminas\InputFilter\InputFilterPluginManager;
 use Psr\Container\ContainerInterface;
 
@@ -17,9 +19,12 @@ final class ConfirmationHandlerFactory
         $pluginManager = $container->get(InputFilterPluginManager::class);
         $inputFilter   = $pluginManager->get(ProfilActivationInputFilter::class);
 
+        $em = $container->get(EntityManagerInterface::class);
+
         return new ConfirmationHandler(
             $container->get(UserServiceInterface::class),
             $inputFilter,
+            $em->getRepository(Entity\User::class)
         );
     }
 }
