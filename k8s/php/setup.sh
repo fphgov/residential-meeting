0883affaec31a940cf33edc057947600a3a670c6
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2023-04-27 14:53:32 +0200

    Moved composer install to Dockerfile
    Signed-off-by: Tamás András Horváth <horvath.tamas.a@budapest.hu>

--- k8s/php/setup.sh
+++ k8s/php/setup.sh
@@ -12,8 +12,6 @@ done
 if [ $MODE = "development" ]; then
   cd /var/www/html
 
-  php -d allow_url_fopen=on /usr/local/bin/composer install
-
   composer db-update
 
   chmod 0644 bin/cron/notifications.php
@@ -21,8 +19,6 @@ fi
 
 if [ $MODE = "production" ]; then
   cd /var/www/html
-
-  php -d allow_url_fopen=on /usr/local/bin/composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader
 
   composer db-update
 
