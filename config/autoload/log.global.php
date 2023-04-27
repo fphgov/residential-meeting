e0c3f88ce5fcdc7d47ffb6a419bbcd9e9f14cfb6
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2023-04-27 10:56:54 +0200

    Remove db logger
    Signed-off-by: Tamás András Horváth <horvath.tamas.a@budapest.hu>

--- config/autoload/log.global.php
+++ config/autoload/log.global.php
@@ -24,42 +24,6 @@ return [
                     ],
                 ],
             ],
-            'db'     => [
-                'name'     => 'db',
-                'priority' => Laminas\Log\Logger::ALERT,
-                'options'  => [
-                    'db'        => new Laminas\Db\Adapter\Adapter([
-                        'driver'   => getenv('DB_DRIVER'),
-                        'database' => getenv('DB_DATABASE'),
-                        'host'     => getenv('DB_HOSTNAME'),
-                        'username' => getenv('DB_USER'),
-                        'password' => getenv('DB_PASSWORD'),
-                        'port'     => getenv('DB_PORT'),
-                    ]),
-                    'table'     => 'log_error',
-                    'column'    => [
-                        'timestamp'    => 'timestamp',
-                        'priority'     => 'priority',
-                        'priorityName' => 'priorityName',
-                        'message'      => 'message',
-                    ],
-                    'formatter' => [
-                        'name'    => Laminas\Log\Formatter\Db::class,
-                        'options' => [
-                            'dateTimeFormat' => 'Y-m-d h:i:s',
-                        ],
-                    ],
-                    'filters'   => [
-                        'priority' => [
-                            'name'    => 'priority',
-                            'options' => [
-                                'operator' => '<=',
-                                'priority' => Laminas\Log\Logger::INFO,
-                            ],
-                        ],
-                    ],
-                ],
-            ],
         ],
         'processors' => [
             'requestid' => [
@@ -70,33 +34,13 @@ return [
     'logger' => [
         'AuditLogger' => [
             'writers'    => [
-                'db'     => [
-                    'name'     => 'db',
+                'stdout' => [
+                    'name'     => 'stream',
                     'priority' => Laminas\Log\Logger::ALERT,
                     'options'  => [
-                        'db'        => new Laminas\Db\Adapter\Adapter([
-                            'driver'   => getenv('DB_DRIVER'),
-                            'database' => getenv('DB_DATABASE'),
-                            'host'     => getenv('DB_HOSTNAME'),
-                            'username' => getenv('DB_USER'),
-                            'password' => getenv('DB_PASSWORD'),
-                            'port'     => getenv('DB_PORT'),
-                        ]),
-                        'table'     => 'log_audit',
-                        'column'    => [
-                            'timestamp'    => 'timestamp',
-                            'priority'     => 'priority',
-                            'priorityName' => 'priorityName',
-                            'message'      => 'message',
-                            'extra'        => [
-                                'extra' => 'extra',
-                            ],
-                        ],
+                        'stream'    => 'php://stdout',
                         'formatter' => [
-                            'name'    => Laminas\Log\Formatter\Db::class,
-                            'options' => [
-                                'dateTimeFormat' => 'Y-m-d h:i:s',
-                            ],
+                            'name'    => Laminas\Log\Formatter\Simple::class,
                         ],
                         'filters'   => [
                             'priority' => [
