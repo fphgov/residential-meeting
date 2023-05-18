cf22a8cfe06f96fa3ef774827fff462d64f7de0f
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2023-05-18 17:38:16 +0200

    Remove unused functions
    Signed-off-by: Tamás András Horváth <horvath.tamas.a@budapest.hu>

--- src/App/src/Repository/MailLogRepository.php
+++ src/App/src/Repository/MailLogRepository.php
@@ -4,45 +4,8 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
-use App\Entity\User;
-use DateTime;
 use Doctrine\ORM\EntityRepository;
-
-use function count;
 
 final class MailLogRepository extends EntityRepository
 {
-    public function getSendedAccountConfirmation(DateTime $date): array
-    {
-        $qb = $this->createQueryBuilder('ml');
-
-        $qb
-            ->where('ml.name = :name')
-            ->andWhere('ml.createdAt >= :date')
-            ->setParameters([
-                'name' => 'account-confirmation',
-                'date' => $date,
-            ]);
-
-        return $qb->getQuery()->getResult();
-    }
-
-    public function isSendedReminder(User $user, DateTime $date): bool
-    {
-        $qb = $this->createQueryBuilder('ml');
-
-        $qb
-            ->where('ml.user = :user')
-            ->andWhere('ml.name = :name')
-            ->andWhere('ml.createdAt >= :date')
-            ->setParameters([
-                'user' => $user,
-                'name' => 'account-confirmation-reminder',
-                'date' => $date,
-            ]);
-
-        $result = $qb->getQuery()->getResult();
-
-        return count($result) > 0;
-    }
 }
