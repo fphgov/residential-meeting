0e2325b7680ecdcf90f9e6aa18edb40904aa74b1
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2023-05-18 17:45:14 +0200

    Remove expired
    Signed-off-by: Tamás András Horváth <horvath.tamas.a@budapest.hu>

--- src/App/src/Repository/MediaRepository.php
+++ src/App/src/Repository/MediaRepository.php
@@ -4,8 +4,22 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use DateTime;
 use Doctrine\ORM\EntityRepository;
 
 final class MediaRepository extends EntityRepository
 {
+    public function getExpiredMedias(DateTime $date): array
+    {
+        $qb = $this->createQueryBuilder('m');
+
+        $qb
+            ->where('m.expirationDate <= :date')
+            ->orWhere('m.expirationDate IS NULL')
+            ->setParameters([
+                'date' => $date,
+            ]);
+
+        return $qb->getQuery()->getResult();
+    }
 }
