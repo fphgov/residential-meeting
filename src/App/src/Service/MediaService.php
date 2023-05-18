4e857290d42663818cbcef4c3b79344fc6d35edd
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2023-05-18 17:45:14 +0200

    Remove expired
    Signed-off-by: Tamás András Horváth <horvath.tamas.a@budapest.hu>

--- src/App/src/Service/MediaService.php
+++ src/App/src/Service/MediaService.php
@@ -6,25 +6,44 @@ namespace App\Service;
 
 use App\Entity\Media;
 use App\Entity\MediaInterface;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Laminas\Diactoros\Stream;
 use Psr\Http\Message\StreamInterface;
 
+use function unlink;
+
 final class MediaService implements MediaServiceInterface
 {
+    private $mediaRepository;
+
     public function __construct(
         private array $config,
         private EntityManagerInterface $em
     ) {
-        $this->config = $config;
-        $this->em     = $em;
+        $this->config          = $config;
+        $this->em              = $em;
+        $this->mediaRepository = $this->em->getRepository(Media::class);
+    }
+
+    public function process(): void
+    {
+        $medias = $this->mediaRepository->getExpiredMedias(new DateTime());
+
+        foreach ($medias as $media) {
+            $filePath = getenv('APP_UPLOAD') . '/' . $media->getFilename();
+
+            unlink($filePath);
+
+            $this->em->remove($media);
+        }
+
+        $this->em->flush();
     }
 
     public function getMedia(string $id): ?MediaInterface
     {
-        $mediaRepository = $this->em->getRepository(Media::class);
-
-        return $mediaRepository->findOneBy(['id' => $id]);
+        return $this->mediaRepository->findOneBy(['id' => $id]);
     }
 
     public function getMediaStream(MediaInterface $media): StreamInterface
