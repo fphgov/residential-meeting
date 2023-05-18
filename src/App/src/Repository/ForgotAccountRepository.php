149a48d6b5f2aa9e26786d48bb47c9aa1b788235
Tamás András Horváth <horvath.tamas.a@budapest.hu>
2023-05-18 17:58:40 +0200

    Add expired forgot account cronjob
    Signed-off-by: Tamás András Horváth <horvath.tamas.a@budapest.hu>

--- src/App/src/Repository/ForgotAccountRepository.php
+++ src/App/src/Repository/ForgotAccountRepository.php
@@ -4,8 +4,21 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use DateTime;
 use Doctrine\ORM\EntityRepository;
 
 class ForgotAccountRepository extends EntityRepository
 {
+    public function getExpiredForgotAccounts(DateTime $date): array
+    {
+        $qb = $this->createQueryBuilder('fa');
+
+        $qb
+            ->where('fa.expirationDate <= :date')
+            ->setParameters([
+                'date' => $date,
+            ]);
+
+        return $qb->getQuery()->getResult();
+    }
 }
